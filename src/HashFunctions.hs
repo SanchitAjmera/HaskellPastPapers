9bf6e0b370f156959ed9d2c95519ae3e7cf29b1c
sanchit <sanchitajmera2017@gmail.com>
2020-07-30 03:48:10 +0100

    partial implementation of part II

--- src/HashFunctions.hs
+++ src/HashFunctions.hs
@@ -4,6 +4,7 @@ import Data.Bits
 import Data.String
 
 import Types
+import Examples
 
 hash :: HashFun
 hash x
@@ -48,3 +49,43 @@ insertAt n insertion list
   = first  ++ (insertion : second)
   where
     (first, second) = splitAt (n) list
+
+sumTrie :: (Int -> Int) -> ([Int] -> Int) -> Trie -> Int
+sumTrie func1 func2 (Leaf leaves)
+  = func2 leaves
+sumTrie func1 func2 (Node bitVector [])
+  = 0
+sumTrie func1 func2 (Node bitVector ((Term int) : subNodes))
+  = func1 int + (sumTrie func1 func2 (Node bitVector subNodes))
+sumTrie func1 func2 (Node bitVector ((SubTrie trie) : subNodes))
+  = (sumTrie func1 func2 trie) + (sumTrie func1 func2 (Node bitVector subNodes))
+
+
+member :: Int -> Hash -> Trie -> Int -> Bool
+member value hashNo trie blockSize
+  = member' value blockSize 0 hashed trie
+  where
+    hashed = hash hashNo
+
+
+member' :: Int -> Int -> Int -> Int -> Trie -> Bool
+member' value blockSize index hashed (Leaf [])
+  = False
+member' value blockSize index hashed (Node bitVector [])
+  = False
+
+member' value blockSize index hashed (Leaf (leaf : leaves))
+  | leaf == value        = True
+  | otherwise            = member' value blockSize index hashed (Leaf leaves)
+
+member' value blockSize index hashed (Node bitVector (Term int : subNodes))
+  | (bitVector == path)  = (int == value)
+  | otherwise            = member' value blockSize index hashed (Node bitVector subNodes)
+  where
+    path = getIndex hashed index blockSize
+
+member' value blockSize index hashed (Node bitVector ((SubTrie trie) : subNodes))
+  | bitVector == path    = member' value blockSize (index+1) hashed trie
+  | otherwise            = member' value blockSize index hashed (Node bitVector subNodes)
+  where
+    path = getIndex hashed index blockSize
